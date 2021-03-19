714daac9c04e460d90f01b860f04c19ba992f466
wook95 <wook@iugchang-ui-MacBookPro.local>
2021-03-19 10:29:29 +0900

    Update MemberController.java

--- src/main/java/com/arc/s1/member/MemberController.java
+++ src/main/java/com/arc/s1/member/MemberController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -33,18 +34,20 @@ public class MemberController {
 	}
 	
 	@RequestMapping(value="/member/memberLogin", method = RequestMethod.POST)
-	public String memberLogin2(HttpServletRequest request) throws Exception {
+	public String memberLogin2(MemberDTO memberDTO, Model model) throws Exception {
 		
-		String id =request.getParameter("id");
-		String pw =request.getParameter("pw");
-		
-		MemberDTO memberDTO = new MemberDTO();
-		memberDTO.setId(id);
-		memberDTO.setPw(pw);
+//		String id =request.getParameter("id");
+//		String pw =request.getParameter("pw");
+//		
+//		MemberDTO memberDTO = new MemberDTO();
+//		memberDTO.setId(id);
+//		memberDTO.setPw(pw);
 		memberDTO=memberService.memberLogin(memberDTO);
-		System.out.println(memberDTO.getEmail());
+//		System.out.println(memberDTO.getEmail());
 		
-		request.setAttribute("dto", memberDTO);
+//		request.setAttribute("dto", memberDTO);
+		model.addAttribute("dto", memberDTO);
+		
 		
 		return "/member/memberPage";
 	}
